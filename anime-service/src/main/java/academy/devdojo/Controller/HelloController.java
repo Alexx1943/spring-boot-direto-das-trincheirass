d2fb30106c464ee818202071f9bfaca4ea80a515
Alex <alextavares1943@gmail.com>
2025-06-08 01:26:12 -0300

    Controller

---
+++ anime-service/src/main/java/academy/devdojo/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package academy.devdojo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "Alex Tavares de Oliveira";
+    }
+
+}
