c2fa69a04f66a651f27a3de4dae7d696a1f46f71
Alex <alextavares1943@gmail.com>
2025-06-08 21:54:22 -0300

    Sprig boot

---
+++ anime-service/src/main/java/academy/devdojo/Controller/AnimeController.java
@@ -0,0 +1,20 @@
+package academy.devdojo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/animes")
+public class AnimeController {
+
+List<String> animes = List.of("1","2","3","4","5");
+
+    @GetMapping
+    public List<String> listAll(){
+        return animes;
+    }
+
+}
--- anime-service/src/main/java/academy/devdojo/Controller/HelloController.java
+++ anime-service/src/main/java/academy/devdojo/Controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hi")
-    public String hi(){
+    public String hi() {
         return "Alex Tavares de Oliveira";
     }
 
