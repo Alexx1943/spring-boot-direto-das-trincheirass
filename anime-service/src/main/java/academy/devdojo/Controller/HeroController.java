c46b880fbd1cf4457e211b9503cee51e47989e9a
Alex <alextavares1943@gmail.com>
2025-06-09 22:35:21 -0300

    Sprig boot

--- anime-service/src/main/java/academy/devdojo/Controller/HeroController.java
+++ anime-service/src/main/java/academy/devdojo/Controller/HeroController.java
@@ -1,9 +1,7 @@
 package academy.devdojo.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import academy.devdojo.Dominio.Pessoa;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,22 +9,33 @@ import java.util.List;
 @RequestMapping("v1/heroes")
 public class HeroController {
 
-    private static final List<String> HEROES = List.of("Guts", "Zoro", "Kakashi", "Goku");
+    private static final List<Pessoa> HEROES = List.of(
+            new Pessoa("Goku", 29,"goku@gmail.com"),
+            new Pessoa("Kakashi", 67,"kakashi@gmail"),
+            new Pessoa("Naruto",42,"nruto@gmail.com"));
 
     @GetMapping
-    public List<String> listaAll(){
+    public List<Pessoa> listaAll(){
         return HEROES;
     }
 
     @GetMapping("filter")
-    public List<String> listAllHeroesParam(@RequestParam(defaultValue = "")String name){
-        return HEROES.stream().filter(hero -> hero.equalsIgnoreCase(name)).toList();
+    public List<Pessoa> listAllHeroesParam(@RequestParam(defaultValue = "")String name){
+        return HEROES.stream().filter(hero -> hero.getName().equals(name)).toList();
     }
 
     @GetMapping("filterList")
-    public List<String> listAllHeroesParamList(@RequestParam List<String> names){
+    public List<Pessoa> listAllHeroesParamList(@RequestParam List<Pessoa> names){
         return HEROES.stream().filter(names::contains).toList();
     }
+
+//    @GetMapping("{name}")
+//    public String findByName(@PathVariable String name){
+//        return HEROES
+//                .stream()
+//                .filter(hero -> hero.getName().equals(name))
+//                .toList();
+//    }
 
 
 
---
+++ anime-service/src/main/java/academy/devdojo/Dominio/Anime.java
@@ -0,0 +1,11 @@
+package academy.devdojo.Dominio;
+
+import java.util.List;
+
+public class Anime {
+
+    private long id;
+    private String name;
+
+
+}
