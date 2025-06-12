967def5f745355c68a11a3fc26dbbef6753dd98f
Alex <alextavares1943@gmail.com>
2025-06-12 19:20:32 -0300

    Spring Boot

--- anime-service/src/main/java/academy/devdojo/Controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/Controller/AnimeController.java
@@ -2,41 +2,44 @@ package academy.devdojo.Controller;
 
 import academy.devdojo.Dominio.Anime;
 import org.springframework.web.bind.annotation.*;
-import org.yaml.snakeyaml.nodes.AnchorNode;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/animes")
+@RequestMapping("v1/animes")
 public class AnimeController {
 
-private static final List<String> animes = List.of("1","2","3","4","5");
+    private static final List<String> animes = List.of("1", "2", "3", "4", "5");
 
     @GetMapping
-    public List<String> listAll(){
+    public List<String> listAlll(){
         return animes;
     }
 
-    @GetMapping("listaAnime")
-    public List<Anime> lista(){
+    @GetMapping("listAll")
+    public List<Anime> listAll() {
         return Anime.listaRetorno();
     }
 
     @GetMapping("findByName")
-    public List<Anime> findByName(@RequestParam(defaultValue = "")String name){
+    public List<Anime> findaByName(@RequestParam String name) {
+
+        if (name == "") return Anime.listaRetorno();
+
         return Anime.listaRetorno()
                 .stream()
                 .filter(anime -> anime.getName().equalsIgnoreCase(name))
                 .toList();
     }
 
-    @GetMapping("findById")
-    public List<Anime> findById(@PathVariable(required = true) Long id){
+    @GetMapping("findById/{id}")
+    public List<Anime> findById(@PathVariable Long id) {
         return Anime.listaRetorno()
                 .stream()
                 .filter(anime -> anime.getId().equals(id))
                 .toList();
     }
+
 
 
 }
--- anime-service/src/main/java/academy/devdojo/Dominio/Anime.java
+++ anime-service/src/main/java/academy/devdojo/Dominio/Anime.java
@@ -21,19 +21,4 @@ public class Anime {
     }
 
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
