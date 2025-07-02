11e125a7824e21803a8b5eee8b7a156f96004136
alex <alextavares1943@gmail.com>
2025-07-01 23:13:05 -0300

    Spring Boot

--- anime-service/src/main/java/academy/devdojo/Repository/ProducerHardCodeRepository.java
+++ anime-service/src/main/java/academy/devdojo/Repository/ProducerHardCodeRepository.java
@@ -16,41 +16,30 @@ import java.util.Optional;
 @Log4j2
 public class ProducerHardCodeRepository {
 
-    private final static List<Producer> PRODUCERS = new ArrayList<>();
     private final Connection connection;
-
-    static {
-
-        var Teste1 = Producer.builder().id(1L).name("Teste1").createdAt(LocalDateTime.now()).build();
-        var Teste2 = Producer.builder().id(2L).name("Teste2").createdAt(LocalDateTime.now()).build();
-        var Teste3 = Producer.builder().id(3L).name("Teste3").createdAt(LocalDateTime.now()).build();
-        var Teste4 = Producer.builder().id(4L).name("Teste4").createdAt(LocalDateTime.now()).build();
-
-        PRODUCERS.addAll(List.of(Teste1, Teste2, Teste3, Teste4));
-
-    }
+    private final ProducerData producers;
 
     public  List<Producer> findAll() {
-        return PRODUCERS;
+        return producers.getProducers();
     }
 
 
     public Optional<Producer> findById(Long id) {
-        return PRODUCERS.stream().filter(producer -> producer.getId().equals(id)).findFirst();
+        return producers.getProducers().stream().filter(producer -> producer.getId().equals(id)).findFirst();
     }
 
     public List<Producer> findByName(String name) {
         log.debug(connection);
-        return PRODUCERS.stream().filter(producer -> producer.getName().equalsIgnoreCase(name)).toList();
+        return producers.getProducers().stream().filter(producer -> producer.getName().equalsIgnoreCase(name)).toList();
     }
 
     public Producer save(Producer producer) {
-        PRODUCERS.add(producer);
+        producers.getProducers().add(producer);
         return producer;
     }
 
     public void delete(Producer producer) {
-        PRODUCERS.remove(producer);
+        producers.getProducers().remove(producer);
     }
 
     public Producer update(Producer producer) {
--- anime-service/src/test/java/academy/devdojo/Repository/ProducerHardCodeRepositoryTest.java
+++ anime-service/src/test/java/academy/devdojo/Repository/ProducerHardCodeRepositoryTest.java
@@ -1,7 +1,24 @@
 package academy.devdojo.Repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+
+@ExtendWith(MockitoExtension.class)
 class ProducerHardCodeRepositoryTest {
 
+    @InjectMocks
+    private ProducerHardCodeRepository repository;
+
+    @Test
+    @DisplayName("findAll returns a list with all producers")
+    void findAllReturnsAllProducers_whenSucessful(){
+        var producer = repository.findAll();
+
+        Assertions.assertThat(producer).isNotNull().hasSize(4);
+    }
 }
