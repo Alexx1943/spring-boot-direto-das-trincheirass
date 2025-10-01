741379fac225c0ef84e63e58fbe3b852a8e27e22
alex <alextavares1943@gmail.com>
2025-10-01 14:19:57 -0300

    Spring boot

--- user-service/src/main/java/academy/devdojo/controller/UserProfileController.java
+++ user-service/src/main/java/academy/devdojo/controller/UserProfileController.java
@@ -1,6 +1,5 @@
 package academy.devdojo.controller;
 
-import academy.devdojo.domain.User;
 import academy.devdojo.mapper.UserMapper;
 import academy.devdojo.response.UserGetResponse;
 import academy.devdojo.service.UserService;
